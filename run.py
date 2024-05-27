79e9e5f3693857d95356b196f34b3c73e6bc95f1
Micha Hesemans <micha_hesemans@hotmail.com>
2024-05-27 21:53:54 +0000

    feat: added apis

--- run.py
+++ run.py
@@ -1,4 +1,21 @@
 import datetime  # imports datatime module to get current time
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('device_quality_audit')
+
+audit = SHEET.worksheet('audit')
+
+data = audit.get_all_values()
 
 
 def gather_device_info():
