09b77f8c4dd733145ca7a9d6944d00df1a2e5dab
Micha Hesemans <micha_hesemans@hotmail.com>
2024-05-27 14:49:41 +0000

    feat: added audit questions

--- run.py
+++ run.py
@@ -1,1 +1,28 @@
 import datetime  # gets current date and time
+
+questions = [
+    ("Is the packaging of the laptops and desktops intact/undamaged?", [
+        "Yes", "No"]),
+    ("Is the device turning on and booting up without issues?", [
+        "Yes", "No"]),
+    ("Is the screen of the device free from dead pixels and cracks?", [
+        "Yes", "No"]),
+    ("Do all the ports (USB, HDMI, audio, etc.) function correctly?",
+        ["Yes", "No"]),
+    ("Are the keyboards and touchpads responsive and free from damage?",
+        ["Yes", "No"]),
+    ("Is the operating system installed and functioning without errors?",
+        ["Yes", "No"]),
+    ("Is the pre-installed software functioning correctly and up to date?",
+        ["Yes", "No"]),
+    ("Does the device meet the specified performance criteria?", [
+        "Yes", "No"]),
+    ("Are the devices free from overheating during operation?",
+        ["Yes", "No"]),
+    ("Does the device connect to Wi-Fi and Ethernet without issues?",
+        ["Yes", "No"]),
+    ("Is Bluetooth and other wireless connectivity options functional?", [
+        "Yes", "No"]),
+    ("Are all devices free from unauthorized modifications or tampering?",
+        ["Yes", "No"])
+]
